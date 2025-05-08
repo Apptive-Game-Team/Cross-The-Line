88050a97817ebfe65dab5328836f2b791ff57371
Vackam <53655740+Vackam@users.noreply.github.com>
2025-05-09 03:26:21 +0900

    feat(#14) : 수락 / 거절 드래그 기능 구현
    RequestObject.* : 오브젝트에 대한 프리팹 구현
    RequestController.cs : 직접적인 움직임에 관한 cs
    그 외 *.* : 씬에서 작업을 위한 정리 및 프리팹 형성

--- CrossTheLine/Assets/Scripts/Request/RequestController.cs
+++ CrossTheLine/Assets/Scripts/Request/RequestController.cs
@@ -1,7 +1,92 @@
-﻿using UnityEngine;
+﻿using CTR.UI;
+using UnityEngine;
+using UnityEngine.EventSystems;
 
+[RequireComponent(typeof(Collider2D))]
 public class RequestController : MonoBehaviour
 {
-    float maxOffSet = rt.rect.width / 3f;
+    public static float DRAG_RATIO = 4f;
+    public static float CORRECTION_VALUE = 1f;
+    private bool isDragging = false;
+    
+    private Vector3 startMouseWorldPosition;
+    private Vector3 startLocalPosition;
+    
+    private float originalLocalX;
+    private float minLocalX;
+    private float maxLocalX;
+   
+    private RequestManager requestManager;
+    void Awake()
+    {
+        requestManager = transform.parent.GetComponent<RequestManager>();
+        
+        startLocalPosition = transform.localPosition;
+        originalLocalX = transform.localPosition.x;
+        minLocalX = originalLocalX - DRAG_RATIO;
+        maxLocalX = originalLocalX + DRAG_RATIO;
+    }
+    
+
+    void OnMouseDown()
+    {
+        var screenPoint = Input.mousePosition;
+        if (Camera.main != null) startMouseWorldPosition = Camera.main.ScreenToWorldPoint(screenPoint);
+
+        isDragging = true;
+    }
+
+    void OnMouseDrag()
+    {
+        if (!isDragging) return;
+        
+        var screenPoint = Input.mousePosition;
+        if (Camera.main != null)
+        {
+            var currentMouseWorldPosition = Camera.main.ScreenToWorldPoint(screenPoint);
+        
+            float deltaX = currentMouseWorldPosition.x - startMouseWorldPosition.x;
+            float clampedX = Mathf.Clamp(startLocalPosition.x + deltaX, minLocalX, maxLocalX);
+            
+        
+            transform.localPosition = new Vector3(clampedX, startLocalPosition.y, startLocalPosition.z);
+        }
+    }
+
+    void OnMouseUp()
+    {
+        isDragging = false;
+        
+        var nowX = transform.localPosition.x;
+        
+        Debug.Log("Drag End");
+
+        // 어떤 시각적인 효과로 넘어갔으면 좋겠다.
+        if (nowX > maxLocalX - CORRECTION_VALUE)
+        {
+            SelectRight();
+        } 
+        else if (nowX < minLocalX + CORRECTION_VALUE)
+        {
+            SelectLeft();
+        }
+        else
+        {
+            transform.localPosition = startLocalPosition;
+        }
+    }
+
+    private void SelectRight()
+    {
+        Debug.Log("SelectRight");
+        requestManager.isAccept = true;
+    }
+
+    private void SelectLeft()
+    {
+        Debug.Log("SelectLeft"); 
+        requestManager.isReject = true;
+    }
+    
     
 }
